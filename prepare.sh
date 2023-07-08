662fbead8e884418a52236e46fd0d8e86aa80069
Vyacheslav <9507548120@mail.ru>
2023-07-08 21:03:02 +0300

    df

--- prepare.sh
+++ prepare.sh
@@ -3,7 +3,7 @@ hostnamectl set-hostname mysql-slave
 #Checking connection
 (ping -c 4 8.8.8.8 1>/dev/nul & echo "conection ok") || echo "connection failed"
 #update
-yum update -y 1>/dev/null
+#yum update -y 1>/dev/null
 
 # install vim
 yum install vim -y 1>/dev/null & echo "vim ok"
