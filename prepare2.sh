c1d3f62f0828181c65d509289bce328a2d5fb150
IVyacheslavA <131446286+IVyacheslavA@users.noreply.github.com>
2023-07-09 09:47:11 +0300

    Update prepare2.sh

--- prepare2.sh
+++ prepare2.sh
@@ -8,3 +8,4 @@ sed -i 's/IPADDR=192.168.1.201/IPADDR=192.168.1.202/' /root/confs/Stend/ifcfg-en
 rm -f /etc/sysconfig/network-scripts/ifcfg-enp0s3
 cp -v /root/confs/Stend/ifcfg-enp0s3 /etc/sysconfig/network-scripts/ifcfg-enp0s3
 ifdown enp0s3; ifup enp0s3
+hostnamectl set-hostname mysql-slave
